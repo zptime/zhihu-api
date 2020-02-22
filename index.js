647cdcc457d07a12b40ebd589ed2a0ca05a59acd
pzhang <pzhang@whtb.tech>
2020-02-22 16:47:36 +0800

    koa-router实现路由

--- index.js
+++ index.js
@@ -1,41 +1,89 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
+// 前缀实现
+const usersRouter = new Router({ prefix: '/users'});
 
-app.use(async (ctx, next)=>{
-    console.log(1);
-    await next();
-    console.log(2);
+// -----koa中间件与洋葱模型-----
+// app.use(async (ctx, next)=>{
+//     console.log(1);
+//     await next();
+//     console.log(2);
+//     ctx.body = 'Hello World, zhihu !';
+// });
 
-    if(ctx.url === '/'){
-        ctx.body = '这是主页'
-    } else if(ctx.url === '/users') {
-        if(ctx.method === 'POST'){
-            ctx.body = '这是用户列表页'
-        } else if(ctx.method === 'GET') {
-            ctx.body = '这是创建用户'
-        } else {
-            ctx.status = 405
-        }
-    } else if(ctx.url.match(/\/users\/\w+/)) {
-        const userId = ctx.url.match(/\/users\/(\w+)/)[1]
-        ctx.body = `这是用户 ${userId}`
-    } else {
-        ctx.status = 404
+// app.use(async (ctx, next)=>{
+//     console.log(3);
+//     await next();
+//     console.log(4);
+// });
+
+// app.use(async (ctx, next)=>{
+//     console.log(5);
+// });
+
+// -----自定义路由中间件-----
+// app.use(async (ctx, next)=>{
+//     if(ctx.url === '/'){
+//         ctx.body = '这是主页'
+//     } else if(ctx.url === '/users') {
+//         if(ctx.method === 'POST'){
+//             ctx.body = '这是用户列表页'
+//         } else if(ctx.method === 'GET') {
+//             ctx.body = '这是创建用户'
+//         } else {
+//             ctx.status = 405
+//         }
+//     } else if(ctx.url.match(/\/users\/\w+/)) {
+//         const userId = ctx.url.match(/\/users\/(\w+)/)[1]
+//         ctx.body = `这是用户 ${userId}`
+//     } else {
+//         ctx.status = 404
+//     }
+// });
+
+// -----koa-router实现路由-----
+// npm i koa-router --save
+
+// 多中间件用法
+const auth = async (ctx, next) => {
+    if(ctx.url !== '/users'){
+        ctx.throw(401)
     }
-    // ctx.body = 'Hello World, zhihu !';
-});
+    await next()
+}
 
-app.use(async (ctx, next)=>{
-    console.log(3);
-    await next();
-    console.log(4);
-});
+router.get('/', (ctx) => {
+    ctx.body = '这是主页'
+})
 
-app.use(async (ctx, next)=>{
-    console.log(5);
-});
+// router.get('/users', (ctx) => {
+//     ctx.body = '这是用户列表页'
+// })
 
-// 输出结果：1 3 5 4 2
+// router.post('/users', (ctx) => {
+//     ctx.body = '创建用户'
+// })
+
+// router.get('/users/:id', (ctx) => {
+//     ctx.body = `这是用户 ${ctx.params.id}`
+// })
+
+usersRouter.get('/', auth, (ctx) => {
+    ctx.body = '这是用户列表页'
+})
+
+usersRouter.post('/', auth, (ctx) => {
+    ctx.body = '创建用户'
+})
+
+usersRouter.get('/:id', auth, (ctx) => {
+    ctx.body = `这是用户 ${ctx.params.id}`
+})
+
+app.use(router.routes())
+app.use(usersRouter.routes())
 
 app.listen(3000);
