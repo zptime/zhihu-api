4d99fc1c15933dfd57901a10b6dbcebae925e807
pzhang <pzhang@whtb.tech>
2020-03-10 13:00:00 +0800

    配置更改

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 // 重要的配置信息：为了安全，这些变量值都应该替换成环境变量，防止信息泄露，外部盗用
 module.exports = {
     secret: 'jwt-secret',
-    connectionStr: 'mongodb://127.0.0.1:27017/mtapp'
+    connectionStr: 'mongodb://127.0.0.1:27017/zhihu'
 }
